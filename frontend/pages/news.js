c2864c9e386e3c5337d8ea529e231f15a7fa145e
kgni <admin@mdia.dk>
2022-08-07 10:32:46 +0200

    remove boilerplate from index, add news site

--- frontend/pages/news.js
+++ frontend/pages/news.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
 
 const news = () => {
-	return <div>news</div>;
+	return (
+		<>
+			<Head>
+				<title>ALZ.NEWS</title>
+				<meta name="description" content="Alz.news this is great news" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<h1 className="text-9xl">News</h1>
+			<Link href="/">
+				<a className="underline">Home</a>
+			</Link>
+		</>
+	);
 };
 
 export default news;
