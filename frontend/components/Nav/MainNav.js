cb93a2cbf1c1695cda3674138ef2d03e8794843f
kgni <admin@mdia.dk>
2022-08-07 20:20:08 +0200

    add fetch for news articles

--- frontend/components/Nav/MainNav.js
+++ frontend/components/Nav/MainNav.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import Link from 'next/link';
-import { motion } from 'framer-motion';
 
 const MainNav = () => {
 	return (
 		<nav className="w-[90%] mx-auto py-4 flex justify-between items-center">
 			<Link href="/">
 				<span className="uppercase font-bold text-3xl cursor-pointer">
-					Alz.<span className="text-xl">news</span>
+					Alz<span>.</span>
+					<span className="text-xl">news</span>
 				</span>
 			</Link>
 			<ul className="flex gap-x-8 font-semibold">
 				<Link href="/">
-					<li className="cursor-pointer">HOME</li>
+					<li className="cursor-pointer hover:text-gray-500 duration-200">
+						HOME
+					</li>
 				</Link>
 				<Link href="/news">
-					<li className="cursor-pointer">NEWS</li>
+					<li className="cursor-pointer hover:text-gray-500 duration-200">
+						NEWS
+					</li>
 				</Link>
 				<Link href="/journals">
-					<li className="cursor-pointer">JOURNALS</li>
+					<li className="cursor-pointer hover:text-gray-500 duration-200">
+						JOURNALS
+					</li>
 				</Link>
 			</ul>
 		</nav>
--- frontend/pages/news.js
+++ frontend/pages/news.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
+import axios from 'axios';
 
 const news = () => {
+	const [news, setNews] = useState(null);
+
+	useEffect(() => {
+		axios.get('http://localhost:8000/api/news').then((response) => {
+			setNews(response.data);
+		});
+	}, []);
+
 	return (
 		<>
 			<Head>
@@ -11,9 +20,15 @@ const news = () => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<h1 className="text-3xl text-center uppercase">News</h1>
-			<Link href="/">
-				<a className="block underline text-center">Home</a>
-			</Link>
+
+			<section className="w-[90%] mx-auto">
+				{news &&
+					news.map((article) => (
+						<a className="hover:underline" target="_blank" href={article.url}>
+							<h4 className="text-xl">{article.title}</h4>
+						</a>
+					))}
+			</section>
 		</>
 	);
 };
