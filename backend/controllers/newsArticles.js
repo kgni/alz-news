75e3ce55d83ef24c42fa975107b3e447c21e7b78
kgni <admin@mdia.dk>
2022-08-21 16:37:00 +0200

    add query limit to /news/ route

--- backend/controllers/newsArticles.js
+++ backend/controllers/newsArticles.js
@@ -3,15 +3,32 @@ const { NewsArticle } = require('../models/resources/newsArticle');
 
 // get all articles
 newsArticlesRouter.get('/news', async (req, res) => {
-	const articles = await NewsArticle.find({});
+	const { limit } = req.query;
 
-	res.json(articles);
+	// parsing limit to check if it is a number
+	const parsedLimit = parseInt(limit);
+
+	// check if we there was a query and if it was a number, if both were true then we send back the amount of articles that were requested
+
+	if (limit && !isNaN(parsedLimit)) {
+		const articles = await NewsArticle.find({})
+			.limit(req.query.limit)
+			.sort({ publishDate: 'desc' });
+		res.json(articles);
+	}
 });
 
+// newsArticlesRouter.get('/news/:id', async (req, res) => {
+// 	const { id } = req.params;
+// 	const articles = await NewsArticle.find({ _id: id });
+
+// 	res.json(articles);
+// });
+
 newsArticlesRouter.get('/news/approved', async (req, res) => {
-	const articles = await NewsArticle.find({ status: 'APPROVED' })
-		.sort({ publishDate: 'desc' })
-		.limit(50);
+	const articles = await NewsArticle.find({ status: 'APPROVED' }).sort({
+		publishDate: 'desc',
+	});
 
 	res.json(articles);
 });
