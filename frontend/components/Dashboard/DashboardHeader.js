2ae047123bc4dd745adcfdf0c38c7e36e501f559
kgni <admin@mdia.dk>
2022-08-26 19:03:21 +0200

    add range functionality

--- frontend/components/Dashboard/DashboardHeader.js
+++ frontend/components/Dashboard/DashboardHeader.js
@@ -6,8 +6,16 @@ import { AiFillCaretDown } from 'react-icons/ai';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 
+function getLastWeeksDate() {
+	const now = new Date();
+
+	return new Date(now.getFullYear(), now.getMonth(), now.getDate() - 7);
+}
+
 const DashboardHeader = ({ articles }) => {
-	const [whenWasArticle, setWhenWasAricle] = useState(null);
+	// get initial date for range
+	const lastWeeksDate = getLastWeeksDate();
+	const [articlesDateRange, setArticlesDateRange] = useState(lastWeeksDate);
 
 	const approvedArticles = articles.filter(
 		(article) => article.status === 'APPROVED'
@@ -18,6 +26,30 @@ const DashboardHeader = ({ articles }) => {
 	const rejectedArticles = articles.filter(
 		(article) => article.status === 'REJECTED'
 	);
+
+	const articlesLastAdded = articles.filter((article) => {
+		return new Date(article.publishDate) > articlesDateRange;
+	});
+
+	const articlesLastAddedApproved = articles.filter((article) => {
+		return (
+			new Date(article.publishDate) > articlesDateRange &&
+			article.status === 'APPROVED'
+		);
+	});
+
+	const articlesLastAddedPending = articles.filter((article) => {
+		return (
+			new Date(article.publishDate) > articlesDateRange &&
+			article.status === 'PENDING'
+		);
+	});
+	const articlesLastAddedRejected = articles.filter((article) => {
+		return (
+			new Date(article.publishDate) > articlesDateRange &&
+			article.status === 'REJECTED'
+		);
+	});
 
 	return (
 		<header className="flex gap-8">
@@ -33,12 +65,24 @@ const DashboardHeader = ({ articles }) => {
 			<div className="bg-zinc-100 py-4 px-8 rounded-lg">
 				{' '}
 				<h3 className="font-bold text-2xl mb-4 flex items-center gap-2">
-					Articles added last week{' '}
+					Added last week{' '}
 					<AiFillCaretDown
 						style={{ marginTop: '5px', cursor: 'pointer' }}
 						size="0.8em"
 					/>
 				</h3>
+				<div className="flex gap-5">
+					<p className="text-green-600 font-bold">
+						{articlesLastAddedApproved.length}
+					</p>
+					<p className="text-orange-300 font-bold">
+						{articlesLastAddedPending.length}
+					</p>
+					<p className="text-red-600 font-bold">
+						{articlesLastAddedRejected.length}
+					</p>
+					<p className="font-bold ml-auto">{articlesLastAdded.length}</p>
+				</div>
 			</div>
 			<div></div>
 			<div></div>
