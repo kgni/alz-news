aa7ab184f9beb657a47330aac66a6eb0a6d5175d
kgni <admin@mdia.dk>
2022-08-28 01:50:24 +0200

    style table add sorting functionality

--- frontend/components/Dashboard/Tables/columns/newsColumns.js
+++ frontend/components/Dashboard/Tables/columns/newsColumns.js
@@ -1,30 +1,139 @@
+import { format, formatDistance, subDays } from 'date-fns';
+import { ImCross } from 'react-icons/im';
+import { AiOutlineCaretDown, AiOutlineCaretUp } from 'react-icons/ai';
+import { FaGlobeEurope } from 'react-icons/fa';
+
 export const NEWS_COLUMNS = [
 	{
 		Header: 'Title',
 		accessor: 'title',
+		Cell: ({ value }) => {
+			if (!value) {
+				return <ImCross style={{ color: 'red' }} />;
+			} else {
+				return `${value.split(' ').slice(0, 5).join(' ')}...`;
+			}
+		},
 	},
 	{
 		Header: 'Subtitle',
 		accessor: 'subtitle',
+		Cell: ({ value }) => {
+			if (!value) {
+				return <ImCross style={{ color: 'red' }} />;
+			} else {
+				return `${value.split(' ').slice(0, 5).join(' ')}...`;
+			}
+		},
 	},
 	{
 		Header: 'Type',
 		accessor: 'type',
+		Cell: ({ value }) => {
+			return 'News';
+		},
 	},
 	{
 		Header: 'Publisher',
 		accessor: 'publisher',
+		Cell: ({ value }) => {
+			// let tagStyle;
+			// switch (value[0]) {
+			// 	case 'The Guardian':
+			// 		tagStyle = 'bg-blue-800 text-white';
+			// 		break;
+			// 	case 'alz.org':
+			// 		tagStyle = 'bg-purple-800 text-white';
+			// 		break;
+			// 	case 'Neuroscience News':
+			// 		tagStyle = 'bg-yellow-400 text-black';
+			// 		break;
+			// 	case 'j-alz.com':
+			// 		tagStyle = 'bg-green-800 text-white';
+			// 		break;
+			// 	case 'alzheimers.org.uk':
+			// 		tagStyle = 'bg-cyan-800 text-white';
+			// 		break;
+			// 	case 'nia.gov':
+			// 		tagStyle = 'bg-red-800 text-white';
+			// 		break;
+			// }
+			// return (
+			// 	<span
+			// 		className={`${tagStyle} p-1 px-4 rounded-full text-xs font-bold inline-flex justify-center`}
+			// 	>
+			// 		{value[0]}
+			// 	</span>
+			// );
+
+			return value[0];
+		},
 	},
 	{
 		Header: 'Publish Date',
 		accessor: 'publishDate',
+		Cell: ({ value }) => {
+			return format(new Date(value), 'dd/MM/yyyy');
+		},
 	},
 	{
 		Header: 'Status',
 		accessor: 'status',
+		Cell: ({ value }) => {
+			if (value === 'PENDING') {
+				return (
+					<span className="p-1 px-4 bg-yellow-400 font-bold text-xs rounded-full inline-flex items-center gap-1 w-[70%] justify-center cursor-pointer">
+						{value}
+						{/* <AiOutlineCaretDown
+							className="ml-auto"
+							style={{ cursor: 'pointer' }}
+						/> */}
+					</span>
+				);
+			}
+
+			if (value === 'APPROVED') {
+				return (
+					<span className="p-1 px-4 bg-[#007C00] font-bold text-xs rounded-full inline-flex items-center gap-1 w-[70%] justify-center text-white cursor-pointer">
+						{value}
+						{/* <AiOutlineCaretDown
+							className="ml-auto"
+							style={{ cursor: 'pointer' }}
+						/> */}
+					</span>
+				);
+			}
+
+			if (value === 'REJECTED') {
+				return (
+					<span className="p-1 px-4 bg-red-600 font-bold text-xs rounded-full inline-flex items-center gap-1 w-[70%] cursor-pointer">
+						{value}
+						<AiOutlineCaretDown
+							className="ml-auto"
+							style={{ cursor: 'pointer' }}
+						/>
+					</span>
+				);
+			}
+		},
 	},
 	{
 		Header: 'Last Edited',
 		accessor: 'updatedAt',
+		Cell: ({ value }) => {
+			return formatDistance(subDays(new Date(value), 3), new Date(), {
+				addSuffix: true,
+			});
+		},
+	},
+	{
+		accessor: 'url',
+		Cell: ({ value }) => {
+			return (
+				<a className="" href={value} target="_blank">
+					<FaGlobeEurope />
+				</a>
+			);
+		},
 	},
 ];
