70cbc0a53908362671f686baec2bbc4d3810e672
kgni <admin@mdia.dk>
2022-09-26 22:01:25 +0200

    add serverside pagination

--- backend/controllers/newsArticles.js
+++ backend/controllers/newsArticles.js
@@ -26,6 +26,63 @@ module.exports = {
 
 		res.json(articles);
 	},
+	getSpecificAmountOfApprovedNewsArticles: async (req, res) => {
+		let { page, size, sortingOrder, filterKeyword, newsSource } = req.query;
+
+		if (!page) {
+			page = 1;
+		}
+
+		if (!size) {
+			size = 12;
+		}
+
+		if (!sortingOrder) {
+			sortingOrder = 'desc';
+		}
+
+		if (!filterKeyword) {
+			filterKeyword = '';
+		}
+
+		const limit = parseInt(size);
+		const skip = (page - 1) * size;
+
+		regexp = new RegExp(filterKeyword, 'i');
+
+		const allArticlesLength = await NewsArticle.find({
+			status: 'APPROVED',
+			title: { $regex: regexp },
+		}).count();
+
+		const articles = await NewsArticle.find({
+			status: 'APPROVED',
+			title: { $regex: regexp },
+		})
+			.sort({
+				publishDate: sortingOrder,
+			})
+			.limit(limit)
+			.skip(skip);
+
+		const recommendedArticles = await NewsArticle.find({
+			recommended: true,
+		});
+
+		const totalPages = Math.ceil(allArticlesLength / size);
+
+		const data = {
+			articles,
+			allArticlesLength,
+			page,
+			totalPages,
+			recommendedArticles,
+		};
+
+		console.log(totalPages);
+
+		res.json(data);
+	},
 	updateNewsArticle: async (req, res) => {
 		try {
 			const id = req.body.id;
