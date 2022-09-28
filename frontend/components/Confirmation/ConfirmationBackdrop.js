7c0cbc0bd9745376ad4441f99b4128706ae9e9cc
kgni <admin@mdia.dk>
2022-09-28 23:21:36 +0200

    fix modal backdrop position for pop-up modal when deleting article

--- frontend/components/Confirmation/ConfirmationBackdrop.js
+++ frontend/components/Confirmation/ConfirmationBackdrop.js
@@ -3,8 +3,8 @@ import React from 'react';
 const ConfirmationBackdrop = ({ children }) => {
 	return (
 		<>
-			<div className="fixed bg-black bg-opacity-60 h-full w-full overflow-hidden z-50 left-72 top-0 "></div>
-			<div className="fixed center-dashboard-popup-modal z-50 top-[30%] rounded-lg flex flex-col">
+			<div className="fixed bg-black bg-opacity-60 h-full w-full overflow-hidden z-40 left-0 top-0 "></div>
+			<div className="fixed center-dashboard-popup-modal z-40 top-[30%] rounded-lg flex flex-col">
 				{children}
 			</div>
 		</>
