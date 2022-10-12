8307d8a86a1ecee6fa6b95ea9a8771651b9f0261
kgni <admin@mdia.dk>
2022-10-12 19:01:47 +0200

    add longer description and image to meta tags

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -64,9 +64,13 @@ export default function HomePage() {
 				<meta name="twitter:card" content="summary" />
 				<meta
 					property="og:description"
-					content="Easy access to Alzheimer's and dementia research"
+					content="Alzh.info is a simple, easy-to-use, non-profit platform for anyone who has an interest in Alzheimer's or Dementia.
+					The purpose is to make resources including articles, journals, practical information and all kinds of knowledge, easily accessible by gathering it all in one place."
 				/>
-				<meta property="og:image" content={'url of image'} />
+				<meta
+					property="og:image"
+					content="https://user-images.githubusercontent.com/84397151/195356267-0c414ae2-846b-4948-a4cd-1cd6c80484a9.png"
+				/>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			{/* this is just to fix the image from overflowing */}
